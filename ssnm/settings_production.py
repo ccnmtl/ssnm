aefd0e12fc77e2d157c030690e4fa19b16d1f389
coati-00 <coati.00@gmail.com>
2015-12-17 17:37:54 -0500

    Revert "convert to ccnmtlsettings"

--- ssnm/settings_production.py
+++ ssnm/settings_production.py
@@ -1,14 +1,51 @@
 # flake8: noqa
 from settings_shared import *
-from ccnmtlsettings.production import common
+import os
 
-locals().update(
-    common(
-        project=project,
-        base=base,
-        STATIC_ROOT=STATIC_ROOT,
-        INSTALLED_APPS=INSTALLED_APPS,
-    ))
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), "templates"),
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ssnm',
+        'HOST': '',
+        'PORT': 6432,
+        'USER': '',
+        'PASSWORD': '',
+    }
+}
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+if 'migrate' not in sys.argv:
+    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+
+STATICMEDIA_MOUNTS = (
+    ('/sitemedia', '/var/www/ssnm/ssnm/sitemedia'),
+)
+
+# AWS Settings for staging
+AWS_QUERYSTRING_AUTH = False
+AWS_STORAGE_BUCKET_NAME = "ccnmtl-ssnm-prod"
+AWS_PRELOAD_METADATA = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+
+# static data, e.g. css, js, etc.
+STATIC_ROOT = "/tmp/ssnm/static"
+STATICFILES_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
+STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_URL = STATIC_URL
+COMPRESS_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
+
+# flash component location
+MEDIA_ROOT = "/var/www/ssnm/ssnm/flash/"
 
 try:
     from local_settings import *
