464bc13e41654b5d0477e3def6b4b1c6ee430c95
Anders Pearson <anders@columbia.edu>
2016-08-15 12:18:22 +0100

    django 1.10 style urls.py

--- ssnm/urls.py
+++ ssnm/urls.py
@@ -1,89 +1,83 @@
+import django.contrib.auth.views
+import djangowind.views
+import flashpolicies.views
+
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from registration.backends.default.views import RegistrationView
-
-
 from ssnm.main.forms import CreateAccountForm
-
+from ssnm.main.views import (
+    get_map, get_map_details, delete_map, show_maps, contact, go_home,
+    display,
+)
 
 admin.autodiscover()
 
 redirect_after_logout = getattr(settings, 'LOGOUT_REDIRECT_URL', None)
 
-auth_urls = (r'^accounts/', include('django.contrib.auth.urls'))
-logout_page = (r'^logout/$',
-               'django.contrib.auth.views.logout',
-               {'next_page': redirect_after_logout})
-admin_logout_page = (r'^accounts/logout/$',
-                     'django.contrib.auth.views.logout',
-                     {'next_page': '/admin/'})
+auth_urls = url(r'^accounts/', include('django.contrib.auth.urls'))
+logout_page = url(r'^logout/$', django.contrib.auth.views.logout,
+                  {'next_page': redirect_after_logout})
+admin_logout_page = url(r'^accounts/logout/$',
+                        django.contrib.auth.views.logout,
+                        {'next_page': '/admin/'})
 
 if hasattr(settings, 'CAS_BASE'):
-    auth_urls = (r'^accounts/', include('djangowind.urls'))
-    logout_page = (r'^logout/$',
-                   'djangowind.views.logout',
-                   {'next_page': redirect_after_logout})
-    admin_logout_page = (r'^admin/logout/$',
-                         'djangowind.views.logout',
-                         {'next_page': redirect_after_logout})
+    auth_urls = url(r'^accounts/', include('djangowind.urls'))
+    logout_page = url(r'^logout/$', djangowind.views.logout,
+                      {'next_page': redirect_after_logout})
+    admin_logout_page = url(r'^admin/logout/$', djangowind.views.logout,
+                            {'next_page': redirect_after_logout})
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     logout_page,
     admin_logout_page,
     auth_urls,
-    url(r'^crossdomain.xml$',
-        'flashpolicies.views.simple',
+    url(r'^crossdomain.xml$', flashpolicies.views.simple,
         {'domains': [settings.STATIC_URL, '*.ccnmtl.columbia.edu']}),
-    (r'^flash/(?P<path>.*)$',
-     'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^flash/(?P<path>.*)$', django.views.static.serve,
+        {'document_root': settings.MEDIA_ROOT}),
     url(r'^accounts/register/$', RegistrationView.as_view(
-        form_class=CreateAccountForm),
-        name='registration_register'),
+        form_class=CreateAccountForm), name='registration_register'),
     url(r'^accounts/password_reset/$',
-        'django.contrib.auth.views.password_reset',
-        name='password_reset'),
+        django.contrib.auth.views.password_reset, name='password_reset'),
     url(r'^accounts/password_reset/done/$',
-        'django.contrib.auth.views.password_reset_done',
+        django.contrib.auth.views.password_reset_done,
         name='password_reset_done'),
     url(r'^accounts/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/'
         '(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        'django.contrib.auth.views.password_reset_confirm',
+        django.contrib.auth.views.password_reset_confirm,
         name='password_reset_confirm'),
     url(r'^accounts/reset/done/$',
-        'django.contrib.auth.views.password_reset_complete',
+        django.contrib.auth.views.password_reset_complete,
         name='password_reset_complete'),
-    (r'^$', 'ssnm.main.views.show_maps'),
-    (r'^help/$', TemplateView.as_view(template_name="help.html"), {},
-     "help-page"),
-    (r'^about/$', TemplateView.as_view(template_name="about.html"), {},
-     "about-page"),
-    (r'^contact/$', 'ssnm.main.views.contact'),
-    (r'^logout/$',
-     'django.contrib.auth.views.logout',
-     {'next_page': '/'}),
-    (r'^thanks/$', TemplateView.as_view(template_name="thanks.html"), {},
-     "thanks-page"),
-    (r'^ecomap/$', 'ssnm.main.views.get_map'),
-    (r'^ecomap/(?P<map_id>\d+)/$', 'ssnm.main.views.get_map'),
-    (r'^ecomap/(?P<map_id>\d+)/display/flashConduit$',
-     'ssnm.main.views.display'),
-    (r'^details/(?P<map_id>\d+)/$', 'ssnm.main.views.get_map_details'),
-    (r'^ecomap/(?P<map_id>\d+)/delete_map/$', 'ssnm.main.views.delete_map'),
-    (r'^ecomap/(?P<map_id>\d+)/display/back_to_list_button_clicked$',
-     'ssnm.main.views.go_home'),
-    (r'^details/$', 'ssnm.main.views.get_map_details'),
-    (r'^accounts/', include('registration.backends.default.urls')),
-    (r'^admin/', include(admin.site.urls)),
-    (r'^stats/$', TemplateView.as_view(template_name="stats.html")),
-    (r'smoketest/', include('smoketest.urls')),
-)
+    url(r'^$', show_maps),
+    url(r'^help/$', TemplateView.as_view(template_name="help.html"),
+        {}, "help-page"),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html"),
+        {}, "about-page"),
+    url(r'^contact/$', contact),
+    url(r'^logout/$',  django.contrib.auth.views.logout, {'next_page': '/'}),
+    url(r'^thanks/$', TemplateView.as_view(template_name="thanks.html"),
+        {}, "thanks-page"),
+    url(r'^ecomap/$', get_map),
+    url(r'^ecomap/(?P<map_id>\d+)/$', get_map),
+    url(r'^ecomap/(?P<map_id>\d+)/display/flashConduit$', display),
+    url(r'^details/(?P<map_id>\d+)/$', get_map_details),
+    url(r'^ecomap/(?P<map_id>\d+)/delete_map/$', delete_map),
+    url(r'^ecomap/(?P<map_id>\d+)/display/back_to_list_button_clicked$',
+        go_home),
+    url(r'^details/$', get_map_details),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^stats/$', TemplateView.as_view(template_name="stats.html")),
+    url(r'smoketest/', include('smoketest.urls')),
+]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns(
-        '',
+    urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    )
+    ]
